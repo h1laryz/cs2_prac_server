70ab6e8825431404ce33cdae0ef43fb3a4e47089
h1laryz <71406854+h1laryz@users.noreply.github.com>
2025-04-11 15:51:03 +0300

    Update install_and_update_server.sh

--- install_and_update_server.sh
+++ install_and_update_server.sh
@@ -201,7 +201,7 @@ rm -f LICENSE
 echo "matchzy_use_pause_command_for_tactical_pause true" >> /home/root/cs2/game/csgo/cfg/MatchZy/config.cfg
 echo "matchzy_enable_tech_pause false" >> /home/root/cs2/game/csgo/cfg/MatchZy/config.cfg
 echo "matchzy_tech_pause_flag \"@css/root\"" >> /home/root/cs2/game/csgo/cfg/MatchZy/config.cfg
-echo "matchzy_minimum_ready_required 0" >> /home/root/cs2/game/csgo/cfg/MatchZy/config.cfg
+echo "matchzy_minimum_ready_required 10" >> /home/root/cs2/game/csgo/cfg/MatchZy/config.cfg
 sleep 1
 
 echo "Adding admins"
